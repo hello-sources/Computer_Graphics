f4ecdfba3819ce407a2282023970a506c20b1527
ltw <3245849061@qq.com>
2020-04-23 23:08:14 +0800

    画点线椭圆

---
+++ 1.图形学实验一/test1_3/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by test1_3.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_TEST1_TYPE                  129
+#define IDD_DIAN                        32771
+#define IDD_XIAN                        32772
+#define IDD_YUAN                        32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ 1.图形学实验一/test1_3/test1_3View.h
@@ -0,0 +1,68 @@
+// test1_3View.h : interface of the CTest1_3View class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_TEST1_3VIEW_H__7E6DD567_8D30_4194_A311_9227D69FE38E__INCLUDED_)
+#define AFX_TEST1_3VIEW_H__7E6DD567_8D30_4194_A311_9227D69FE38E__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+class CTest1_3View : public CView
+{
+protected: // create from serialization only
+	CTest1_3View();
+	DECLARE_DYNCREATE(CTest1_3View)
+
+// Attributes
+public:
+	CTest1_3Doc* GetDocument();
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CTest1_3View)
+	public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CTest1_3View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CTest1_3View)
+	afx_msg void OnDian();
+	afx_msg void OnXian();
+	afx_msg void OnYuan();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // debug version in test1_3View.cpp
+inline CTest1_3Doc* CTest1_3View::GetDocument()
+   { return (CTest1_3Doc*)m_pDocument; }
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_TEST1_3VIEW_H__7E6DD567_8D30_4194_A311_9227D69FE38E__INCLUDED_)
